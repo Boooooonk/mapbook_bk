2f31ee09ab864ea5c87a950cd25b07c9db3a7148
student <szsendrowski@gmail.com>
2025-04-07 10:14:55 +0200

    chore: code structure updated

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-users: list[dict] = [
-    {'name': 'Bartlomiej', 'location': 'Krynki', 'posts': 500},
-    {'name': 'Karol', 'location': 'Miedzyrzec Podlaski', 'posts': 700},
-    {'name': 'Krystian', 'location': 'Zyrardow', 'posts': 200}
-]
+from Utills.controller import get_user_info
+from Utills.model import users
 
+def main():
+    get_user_info(users)
 
-def get_user_info(users_data: list[dict]) -> None:
-    for user in users:
-        print(f'Twój znajomy: {user["name"]} z miejscowości: {user["location"]} opublikował {user["posts"]} postów.')
-
-get_user_info(users)
+if __name__ == "__main__":
+    main()
