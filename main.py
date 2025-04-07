9b3957325e77b333a32b969a3fd4d8340eaa8077
student <szsendrowski@gmail.com>
2025-04-07 11:16:13 +0200

    wip: simple menu options

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from Utills.controller import get_user_info
+from Utills.controller import get_user_info, add_user
 from Utills.model import users
 
 
@@ -7,15 +7,17 @@ def main():
         print('================Menu================')
         print('0 - Exit')
         print('1 - Get user info')
+        print('2 - Add user')
         print('====================================')
+
         choice = int(input('Podaj numer z menu: '))
         if choice == 0:
             break
         elif choice == 1:
             get_user_info(users)
             break
+        elif choice == 2:
+            add_user(users)
 
-
-#if __name__ == "__main__":
-#    main()
-
+if __name__ == "__main__":
+    main()
