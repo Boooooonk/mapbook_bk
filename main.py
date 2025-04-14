8dd94ed0260e8c625f32090cef2178cb2d52bf5b
student <>
2025-04-14 09:11:02 +0200

    wip: new menu option: remove users

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from Utills.controller import get_user_info, add_user
+from Utills.controller import get_user_info, add_user, remove_user
 from Utills.model import users
 
 
@@ -8,6 +8,7 @@ def main():
         print('0 - Exit')
         print('1 - Get user info')
         print('2 - Add user')
+        print('3 - Remove user')
         print('====================================')
 
         choice = int(input('Podaj numer z menu: '))
@@ -15,9 +16,10 @@ def main():
             break
         elif choice == 1:
             get_user_info(users)
-            break
         elif choice == 2:
             add_user(users)
+        elif choice == 3:
+            remove_user(users)
 
 if __name__ == "__main__":
     main()
